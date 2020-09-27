8b0b9df95068a8869d184d4d22d35ee22df141ef
Fernandes <nathalia.fernandes@sap.com>
2020-09-27 20:39:46 -0300

    Adding API validations and adjusting data files

--- src/test/java/constants/ProjectConstants.java
+++ src/test/java/constants/ProjectConstants.java
@@ -5,4 +5,5 @@ import java.io.File;
 public class ProjectConstants {
     public static final String CURRENT_PATH = new File("").getAbsolutePath();
     public static final String REPORT_PATH = CURRENT_PATH + "/reports/";
+    public static final String URL = "https://api.ratesapi.io/api/";
 }
